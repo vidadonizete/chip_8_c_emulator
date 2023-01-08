15b08ab06ecbfee7693d003db00f2e4f84907c04
empty <empty>
2023-01-08 20:09:07 -0300

    It's rendering

--- main.c
+++ main.c
@@ -44,7 +44,7 @@ struct chip_8
 
 static void read_rom(struct chip_8 *chip_8, char *name)
 {
-    FILE *file = fopen("../ibm.ch8", "rb");
+    FILE *file = fopen(name, "rb");
     if (!file)
     {
         fprintf(stderr, "Error opening %s", name);
@@ -53,11 +53,36 @@ static void read_rom(struct chip_8 *chip_8, char *name)
     fseek(file, 0L, SEEK_END);
     int bytes = ftell(file);
     rewind(file);
-    if (!fread(chip_8->memory + ROM_OFFSET, bytes, 1, file))
+    if (!fread(chip_8->memory + ROM_OFFSET, bytes, sizeof(uint8_t), file))
     {
         fprintf(stderr, "Error reading %s", name);
         exit(0);
     }
+    fclose(file);
+}
+
+static void clearScreen(SDL_Renderer* renderer)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+}
+
+static void drawPixels(SDL_Renderer *renderer, uint8_t display[2048])
+{
+    clearScreen(renderer);
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COLUMN; j++)
+        {
+            if (display[i * COLUMN + j])
+            {
+                SDL_RenderDrawPoint(renderer, j, i);
+            }
+        }
+    }
+    SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char *argv[])
@@ -69,22 +94,20 @@ int main(int argc, char *argv[])
     }
 
     SDL_Window *window;
-    SDL_Renderer* renderer;
+    SDL_Renderer *renderer;
 
     if (SDL_CreateWindowAndRenderer(
-        COLUMN,
-        ROW,
-        SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN,
-        &window,
-        &renderer
-    ) < 0) {
+            COLUMN,
+            ROW,
+            SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN,
+            &window,
+            &renderer) < 0)
+    {
         fprintf(stderr, "Error starting SDL2: %s", SDL_GetError());
         exit(0);
     }
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawPoint(renderer, 32, 16);
-    SDL_RenderPresent(renderer);
+    clearScreen(renderer);
 
     struct chip_8 chip_8 = {
         .memory = {},
@@ -97,40 +120,44 @@ int main(int argc, char *argv[])
         .sound = 0,
         .display = {},
     };
-    read_rom(&chip_8, "../ibm.ch8");
+
+    read_rom(&chip_8, "../test.ch8");
     memcpy(chip_8.memory + FONT_OFFSET, font, sizeof(font));
 
     uint16_t opcode = 0;
     uint8_t left = 0;
     uint8_t right = 0;
+    uint8_t code = 0;
 
     while (1)
     {
         left = chip_8.memory[chip_8.pc++];
         right = chip_8.memory[chip_8.pc++];
         opcode = (left << 8) | right;
+        code = left >> 4;
 
         if (opcode == 0x00E0) // 00E0 (clear screen)
         {
             memset(chip_8.display, 0, sizeof(chip_8.display));
+            clearScreen(renderer);
         }
-        else if (opcode & 0x1000) // 1NNN (jump)
+        else if (code == 0x1) // 1NNN (jump)
         {
             chip_8.pc = opcode & 0x0FFF;
         }
-        else if (opcode & 0x6000) // 6XNN (set register VX)
+        else if (code == 0x6) // 6XNN (set register VX)
         {
             chip_8.registers[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
         }
-        else if (opcode & 0x7000) // 7XNN (add value to register VX)
+        else if (code == 0x7) // 7XNN (add value to register VX)
         {
             chip_8.registers[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
         }
-        else if (opcode & 0xA000) // ANNN (set index register I)
+        else if (code == 0xA) // ANNN (set index register I)
         {
             chip_8.index = opcode & 0x0FFF;
         }
-        else if (opcode & 0xD000) // DXYN (display/draw)
+        else if (code == 0xD) // DXYN (display/draw)
         {
             chip_8.registers[0x0F] = 0;
 
@@ -142,17 +169,16 @@ int main(int argc, char *argv[])
 
             for (uint8_t r = 0; r < n && r + y < ROW; r++)
             {
+                uint8_t *row = chip_8.display + ((y + r) * COLUMN);
                 for (uint8_t c = 0; c < 8 && c + x < COLUMN; c++)
                 {
                     uint8_t pixel = (*sprite >> (7 - c)) & 1;
-                    chip_8.display[(y + r) * COLUMN + (x + c)] = pixel;
+                    *(row + x + c) = pixel;
                 }
                 sprite++;
             }
-        }
-        else
-        {
-            printf("Not implemented: 0x%08x\n", opcode);
+
+            drawPixels(renderer, chip_8.display);
         }
     }
 
